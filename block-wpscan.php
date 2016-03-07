db63cf346c673e03cc4bf56f9271ec4a2ff32d90
rluisr <detteiu.x14th@gmail.com>
2016-03-07 21:54:40 +0900

    UI

--- block-wpscan.php
+++ block-wpscan.php
@@ -42,6 +42,7 @@ function register_frontend()
 {
     wp_register_script('bootstrap_js', plugin_dir_url(__FILE__) . 'assets/js/bootstrap.min.js', array(), NULL, false);
     wp_register_style('bootstrap_css', plugin_dir_url(__FILE__) . 'assets/css/bootstrap.min.css', array(), NULL, false);
+    wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrap_js');
     wp_enqueue_style('bootstrap_css');
 }
@@ -60,7 +61,7 @@ function admin_block_wpscan()
 
 function menu_block_wpscan()
 {
-    if (isset($_POST['msg']) && $_POST['proxy'] && $_POST['tor'] && check_admin_referer('check_referer')) {
+    if (isset($_POST['msg']) && check_admin_referer('check_referer')) {
         update_option('msg', esc_html(htmlspecialchars(filter_input(INPUT_POST, 'msg', FILTER_SANITIZE_SPECIAL_CHARS), ENT_QUOTES)));
         update_option('proxy', esc_html(htmlspecialchars(filter_input(INPUT_POST, 'proxy', FILTER_SANITIZE_SPECIAL_CHARS), ENT_QUOTES)));
         update_option('tor', esc_html(htmlspecialchars(filter_input(INPUT_POST, 'tor', FILTER_SANITIZE_SPECIAL_CHARS), ENT_QUOTES)));
@@ -73,11 +74,12 @@ function menu_block_wpscan()
     $tor = get_option('tor');
     $ip = get_option('ip');
     $log = get_option('log');
-    $wp_n = wp_ngonce_field('check_referer');
+    $wp_n = wp_nonce_field('check_referer');
 
     echo <<<HTML
-    <div class="container-fluid">
     <h1>block-wpscan</h1>
+
+    <hr>
 
     <ul class="nav nav-tabs">
         <li class="active"><a href="#tab1" data-toggle="tab">Setting</a></li>
@@ -85,80 +87,95 @@ function menu_block_wpscan()
     </ul>
 
 <!-- START Setting PAGE -->
-    <div class="tab-content">
-        <div class="tab-pane active" id="tab1">
-            <form action="" method="post">
-            ${wp_n}
-            <div class="form-group">
-                <h3>What message do you want to display, when the access is blocked.</h3>
-                <p>Example: Fuck U</p>
-                <input type="text" name="msg" value="${msg}">
-            </div>
+    <div class="container-fluid">
+        <div class="tab-content">
+            <div class="tab-pane active" id="tab1">
+                <div class="row">
+                    <div class="col-sm-7">
+                        <form action="" method="post" >
+                        ${wp_n}
+                            <div class="form-group">
+                                <h3>What message do you want to display, when the access is blocked.</h3>
+                                    <textarea class="form-control" type="text" name="msg" placeholder="Example: Fuck You !">${msg}</textarea>
+                                <p class="help-block">Can't use HTML. Coming soon.</p>
+                            </div>
+
 
             <br>
 
             <div class="form-group">
                 <h3>Block Proxy ON / OFF</h3>
+                <label class="radio-inline">
 HTML;
                 echo $proxy == "ON" ? "<input type=\"radio\" name=\"proxy\" value=\"ON\" checked>ON" : "<input type=\"radio\" name=\"proxy\" value=\"ON\">ON";
+                echo "</label>";
+                echo "<label class=\"radio-inline\">";
                 echo $proxy == "OFF" ? "<input type=\"radio\" name=\"proxy\" value=\"OFF\" checked>OFF" : "<input type=\"radio\" name=\"proxy\" value=\"OFF\">OFF";
     echo <<<HTML
+                </label>
             </div>
 
             <br>
 
             <div class="form-group">
                 <h3>Block Tor ON / OFF</h3>
-                <p>If you check ON, It takes a bit of a while load time. Please test.</p>
+                <h5>If you check ON, It takes a bit of a while load time. Please test.</h5>
+                <label class="radio-inline">
 HTML;
                 echo $tor == "ON" ? "<input type=\"radio\" name=\"tor\" value=\"ON\" checked>ON" : "<input type=\"radio\" name=\"tor\" value=\"ON\">ON";
+                echo "</label>";
+                echo "<label class=\"radio-inline\">";
                 echo $tor == "OFF" ? "<input type=\"radio\" name=\"tor\" value=\"OFF\" checked>OFF" : "<input type=\"radio\" name=\"tor\" value=\"OFF\">OFF";
     echo <<<HTML
+                </label>
             </div>
 
             <br>
 
             <div class="form-group">
                 <h3>Exception IP</h3>
-                <p>If you have many exception IPs,Please sprit with ","<br>
+                <h5>If you have many exception IPs,Please sprit with ","<br>
                 You should add server's ip<global> for other plugins. ex)Broken Link Checker<br>
-                Example: 1.1.1.1,2.2.2.2,3.3.3.3</p>
-                <input type="text" name="ip" value="${ip}">
+                Example: 1.1.1.1,2.2.2.2,3.3.3.3</h5>
+                    <input class="form-control" type="text" name="ip" value="${ip}">
             </div>
 
             <br>
 
             <div class="form-group">
                 <h3>Log fnction</h3>
-                <p>If you check on, It takes a bit of a while load time. Please test.</p>
+                <h5>If you check on, It takes a bit of a while load time. Please test.</h5>
+                <label class="radio-inline">
 HTML;
                 echo $log == "ON" ? "<input type=\"radio\" name=\"log\" value=\"ON\" checked>ON" : "<input type=\"radio\" name=\"log\" value=\"ON\">ON";
+                echo "</label>";
+                echo "<label class=\"radio-inline\">";
                 echo $log == "OFF" ? "<input type=\"radio\" name=\"log\" value=\"OFF\" checked>OFF" : "<input type=\"radio\" name=\"log\" value=\"OFF\">OFF";
 
     echo <<<HTML
+                </label>
             </div>
 
             <br>
 
-            <input class="btn btn-default" type="submit" value="Save all">
-            </form>
-
-    <br>
-    <br>
-
-    <p>This plugin is developing.<p>
-    <p>----------------------------------------------------------------------------------------------</p >
-    <p>If you have any problems or requests, Please contact me <a href="https://twitter.com/lu_iskun">@lu_iskun</a> or <a href="https://github.com/rluisr/block-wpscan">github</a>.</p>
-</div>
-<!-- END Setting PAGE -->
-HTML;
-        echo "<div class=\"tab-pane\" id=\"tab2\">";
-            getLog();
-        echo "</div>";
-    echo <<<HTML
+                    <input class="btn btn-default" type="submit" value="Save all">
+                </form>
+            </div>
+            <div class="col-sm-1">
+                <div style="background-color:gray; width:2px; height:100%;"></div>
+            </div>
+            <div class="col-sm-4">
+                <p>Here</p>
+            </div>
+        </div>
     </div>
-</div>
+<!-- END Setting PAGE -->
 
+<!-- START Log PAGE -->
+        <div class="tab-pane" id="tab2">
+            <h1></h1>
+        </div>
+    </div>
 HTML;
 }
 
@@ -170,9 +187,7 @@ function block_wpscan()
      */
     $result = 1;
 
-    /**
-     * IP - Tor
-     */
+    /* IP - Tor */
     if (get_option('tor') == "ON") {
         $url = 'https://c.xzy.pw/judgementAPI-for-Tor/api.php';
         $ip = $_SERVER['REMOTE_ADDR'];
@@ -191,11 +206,7 @@ function block_wpscan()
         $result = json_decode(file_get_contents($url, false, $context));
         $tor_result = $result === null ? 1 : $result->result;
     }
-    /**
-     * Exception IP
-     * 自分自身のアクセスは例外（忘れてたンゴｗ）
-     * サーバー自身のAPIを取得するのもAPI依存だからエラー処理
-     */
+    /* Exception IP */
     $exception_result = $_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR'] ? 1 : 0;
     if (get_option('ip') || $exception_result === 0) {
         $exception_ip = explode(",", get_option('ip'));
@@ -210,9 +221,7 @@ function block_wpscan()
             }
         }
     }
-    /**
-     * Browser's languages
-     */
+    /* Browser's languages */
     if (filter_input(INPUT_SERVER, 'HTTP_ACCEPT_LANGUAGE', FILTER_SANITIZE_SPECIAL_CHARS)) {
         $languages = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
         $languages = array_reverse($languages);
@@ -230,13 +239,9 @@ function block_wpscan()
     } else {
         $browser_result = 0;
     }
-    /**
-     * Googlebot
-     */
+    /* Googlebot */
     $bot_result = strpos(gethostbyaddr($_SERVER['REMOTE_ADDR']), "google.com") === false ? $bot_result = 0 : $bot_result = 1;
-    /**
-     * User-Agent
-     */
+    /* UserAgent */
     if (filter_input(INPUT_SERVER, 'HTTP_USER_AGENT', FILTER_SANITIZE_SPECIAL_CHARS)) {
         if (strpos($_SERVER['HTTP_USER_AGENT'], "Mozilla") === false) {
             $ua_result = 0;
@@ -246,9 +251,7 @@ function block_wpscan()
     } else {
         $ua_result = 0;
     }
-    /**
-     * Header - Proxy
-     */
+    /* Header - Proxy */
     if (get_option('proxy') == "ON") {
         $proxy_result1 = isset($_SERVER['HTTP_VIA']) ? 0 : 1;
         $proxy_result2 = isset($_SERVER['HTTP_CLIENT_IP']) ? 0 : 1;
@@ -269,3 +272,9 @@ function block_wpscan()
         die(esc_html(get_option('msg')));
     }
 }
+
+/*
+ * 0.1.1はログと、UIの改善
+ * 0.2.1はメッセージ表示機能
+ * 0.3.1はHTMLの表示、リダイレクトの機能
+ */
